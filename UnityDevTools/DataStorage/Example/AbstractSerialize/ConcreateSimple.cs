a41cfb1a8c40b4ae461a289d34915f43ce8fe2d0
zinchenko.artem <zinchenko.artem1996@gmail.com>
2020-09-27 17:44:55 +0300

    minor fix

--- UnityDevTools/DataStorage/Example/AbstractSerialize/ConcreateSimple.cs
+++ UnityDevTools/DataStorage/Example/AbstractSerialize/ConcreateSimple.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 [Serializable]
 public class ConcreateSimple : IItem
 {
+#pragma warning disable CS0649
     [SerializeField]
     private string m_id;
     [SerializeField]
     private double m_value;
     [SerializeField]
     private bool m_isEquipable;
+
+#pragma warning restore CS0649
 
     public int OwnValue;
     public string Id { get => m_id; set => m_id=value; }
